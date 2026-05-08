49855a910009a8c5de967726096fc71747b7469e
hadzhiDev <hm03.business@gmail.com>
2026-05-08 16:38:09 +0600

    Paginations and Viewset, router

--- api/viewsets.py
+++ api/viewsets.py
@@ -1,15 +1,18 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
 
 from catalog.models import Movie, Category, Genre, MovieImage
 from .serializers import (
     MovieModelSerializer, MovieSerializer, 
     MovieCreateSerializer, MovieDetailSerializer
 )
+from .paginations import StandardResultsSetPagination
 
 
 class MovieViewSet(ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieModelSerializer
+    pagination_class = StandardResultsSetPagination
 
     def get_serializer_class(self):
         if self.action == "create":
